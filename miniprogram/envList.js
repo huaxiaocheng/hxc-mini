e9bade5850e9f77454eb92d1017b187ad2412b3a
xiaocheng hua <xiaocheng.hua@pacteraedge.com>
2022-02-08 14:50:44 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-1gs18lyh24db0016","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-1gs18lyh24db0016 --n quickstartFunctions --r --project "D:\workspace\hxc-mini" --report_first --report
